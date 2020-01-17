c14ef8266fdb2ccd554c1a69ad7f8066a38ae1a8
Robson Sousa <robsoncartes@outlook.com>
2020-01-16 21:51:20 -0300

    Endpoint '/accounts/{id}' da classe AccountController disponível.

---
+++ src/main/java/br/edu/fatecsjc/controllers/AccountController.java
@@ -0,0 +1,26 @@
+package br.edu.fatecsjc.controllers;
+
+import br.edu.fatecsjc.models.Account;
+import br.edu.fatecsjc.services.impl.AccountServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("AccountController")
+@RequestMapping(value = "/accounts")
+public class AccountController {
+
+    @Autowired
+    private AccountServiceImpl accountService;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Account> findAccountById(@PathVariable Long id) {
+
+        Account account = accountService.findById(id);
+
+        return ResponseEntity.ok().body(account);
+    }
+}
