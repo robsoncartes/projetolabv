8bb639d183a8301d087f56ee20761e9cb570aecc
Robson Sousa <robsoncartes@outlook.com>
2020-05-04 19:32:16 -0300

    Criação da classe de teste 'AdministratorTest' e seus respectivos métodos de testPojo ('Constructor', 'Getter', 'Setter', 'HashCode', 'Equals' e 'toString').

---
+++ src/test/java/br/edu/fatecsjc/unit/models/AdministratorTest.java
@@ -0,0 +1,59 @@
+package br.edu.fatecsjc.unit.models;
+
+import br.edu.fatecsjc.models.Administrator;
+import br.edu.fatecsjc.utils.TestUtil;
+import org.junit.Test;
+import pl.pojo.tester.api.assertion.Method;
+
+import static pl.pojo.tester.api.assertion.Assertions.assertPojoMethodsFor;
+
+public class AdministratorTest extends TestUtil {
+
+    @Test
+    public void administratorTestPojoConstructors() {
+
+        final Class<?> classUnderTest = Administrator.class;
+
+        assertPojoMethodsFor(classUnderTest).testing(Method.CONSTRUCTOR).areWellImplemented();
+    }
+
+    @Test
+    public void administratorTestPojoGetters() {
+
+        final Class<?> classUnderTest = Administrator.class;
+
+        assertPojoMethodsFor(classUnderTest).testing(Method.GETTER).areWellImplemented();
+    }
+
+    @Test
+    public void administratorTestPojoSetters() {
+
+        final Class<?> classUnderTest = Administrator.class;
+
+        assertPojoMethodsFor(classUnderTest).testing(Method.SETTER).areWellImplemented();
+    }
+
+    @Test
+    public void administratorTestPojoHashCode() {
+
+        final Class<?> classUnderTest = Administrator.class;
+
+        assertPojoMethodsFor(classUnderTest).testing(Method.HASH_CODE).areWellImplemented();
+    }
+
+    @Test
+    public void administratorTestPojoEquals() {
+
+        final Class<?> classUnderTest = Administrator.class;
+
+        assertPojoMethodsFor(classUnderTest).testing(Method.EQUALS).areWellImplemented();
+    }
+
+    @Test
+    public void administratorTestPojoToString() {
+
+        final Class<?> classUnderTest = Administrator.class;
+
+        assertPojoMethodsFor(classUnderTest).testing(Method.TO_STRING).areWellImplemented();
+    }
+}
