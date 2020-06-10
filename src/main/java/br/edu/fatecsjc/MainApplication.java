62366cffd1bde0acd9bb183131c93179972a3ade
Robson Sousa <robsoncartes@outlook.com>
2020-06-10 04:45:19 -0300

    Substituição da implementação da interface 'CommandLineRunner' pela extensão da classe 'SpringBootServletInitializer' para implementação WAR tradicional (not embbeded).

--- src/main/java/br/edu/fatecsjc/MainApplication.java
+++ src/main/java/br/edu/fatecsjc/MainApplication.java
@@ -1,19 +1,15 @@
 package br.edu.fatecsjc;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApplication implements CommandLineRunner {
+public class MainApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 
         SpringApplication.run(MainApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-
-    }
 }
